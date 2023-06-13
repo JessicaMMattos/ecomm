4f1e8871e6e6eb96d8740f3433e4ffc9b76a78cc
JessicaMMattos <jessicamartins0563@gmail.com>
2023-06-13 18:15:01 -0300

    Node.js: Detalhar uma Categoria

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -15,4 +15,20 @@ export default class CategoryService {
             return "Erro, não foi possível encontrar a lista de categorias!";
         }    
     }
+
+    static async findCategoryById(idCategoria) {
+
+        const response = await fetch(`http://localhost:3000/categories/${idCategoria}`);
+        const status = response.status;
+
+        console.log(`response status: ${status}`);
+
+        if(status == 200){
+            const categoria = await response.json();   
+            return categoria;
+            
+        } else {
+            return "Erro, não foi possível encontrar a categoria.";
+        }    
+    }
 }
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -8,6 +8,15 @@ async function processarComando(argumentos) {
             const categorias = await CategoryService.findCategories();
             console.log(categorias);
             break;
+
+        case '--recuperarCategoriaPorId':
+            const idCategoria = argumentos[3];
+            const categoriaEspecifica = await CategoryService.findCategoryById(idCategoria);
+            console.log(categoriaEspecifica);
+            break;
+
+        default:
+            console.log(`Não foi possível identificar o comando: ${comando}.`);
     }
 }
 
