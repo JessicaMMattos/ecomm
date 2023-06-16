c6433d81eca562276ee4d9ea05789a7d85a2d12e
JessicaMMattos <jessicamartins0563@gmail.com>
2023-06-16 12:34:30 -0300

    Node.js: Configuração do ESLint e correção de erros

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -3,37 +3,40 @@ import fs from 'fs';
 
 async function processarComando(argumentos) {
     const comando = argumentos[2];
+    let idCategoria;
+    let arquivoNovaCategoria, arquivoCategoriaAtualizada, conteudoCategoriaAtualizada;
+    let categorias, categoriaEspecifica, novaCategoria, categoriaAtualizada, categoriaExcluida;
 
     switch (comando) {
         case '--listarCategorias':
-            const categorias = await CategoryService.findCategories();
+            categorias = await CategoryService.findCategories();
             console.log(categorias);
             break;
 
         case '--recuperarCategoriaPorId':
-            const idCategoria = argumentos[3];
-            const categoriaEspecifica = await CategoryService.findCategoryById(idCategoria);
+            idCategoria = argumentos[3];
+            categoriaEspecifica = await CategoryService.findCategoryById(idCategoria);
             console.log(categoriaEspecifica);
             break;
 
         case '--inserirCategoria':
-            const arquivoNovaCategoria = argumentos[3];
-            const novaCategoria = await CategoryService.createCategory(arquivoNovaCategoria);
+            arquivoNovaCategoria = argumentos[3];
+            novaCategoria = await CategoryService.createCategory(arquivoNovaCategoria);
             console.log(novaCategoria);
             break;
 
         case '--atualizarCategoria':
-            const idAtualizarCategoria = argumentos[3];
-            const arquivoCategoriaAtualizada = argumentos[4];
-            const conteudoCategoriaAtualizada = await fs.promises.readFile(arquivoCategoriaAtualizada, 'utf-8');
+            idCategoria = argumentos[3];
+            arquivoCategoriaAtualizada = argumentos[4];
+            conteudoCategoriaAtualizada = await fs.promises.readFile(arquivoCategoriaAtualizada, 'utf-8');
 
-            const categoriaAtualizada = await CategoryService.updateCategory(idAtualizarCategoria, conteudoCategoriaAtualizada);
+            categoriaAtualizada = await CategoryService.updateCategory(idCategoria, conteudoCategoriaAtualizada);
             console.log(categoriaAtualizada);
             break;
 
         case '--excluirCategoria':
-            const idExcluirCategoria = argumentos[3];
-            const categoriaExcluida = await CategoryService.deleteCategory(idExcluirCategoria);
+            idCategoria = argumentos[3];
+            categoriaExcluida = await CategoryService.deleteCategory(idCategoria);
             console.log(categoriaExcluida);
             break;
 
