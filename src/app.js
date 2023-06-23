1323ef4d281cc81212840b105ce992c0ab7acc0a
JessicaMMattos <jessicamartins0563@gmail.com>
2023-06-23 17:16:30 -0300

    Expondo a documentação nas APIs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from 'yaml';
+import fs from 'fs';
+
+const file  = fs.readFileSync('./swagger/ecomm.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 
@@ -11,5 +17,6 @@ db.once("open", () => {
 const app = express();
 app.use(express.json());
 routes(app);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
